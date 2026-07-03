50f18ac8ed1690ce7eaa847787eba9631d6e164b
sam03-sam03 <samprithanb09@gmail.com>
2026-07-03 14:27:08 +0530

    Add student placement prediction functionality

--- New folder (2)/app.py
+++ New folder (2)/app.py
@@ -1,1 +1,19 @@
+import streamlit as st
+import joblib
+import numpy as np
 
+model = joblib.load('placement_model.pkl')
+
+st.title("Student Placement Predictor")
+
+internships = st.number_input("Number of Internships", min_value=0, max_value=5, value=1)
+cgpa = st.number_input("CGPA", min_value=0.0, max_value=10.0, value=7.5)
+backlogs = st.selectbox("History of Backlogs", [0, 1])
+
+if st.button("Predict"):
+    features = np.array([[internships, cgpa, backlogs]])
+    prediction = model.predict(features)[0]
+    if prediction == 1:
+        st.success("Likely to be Placed! 🎉")
+    else:
+        st.error("Likely Not Placed")
